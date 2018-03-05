e9469ff89543469915b1ee0eb2ecb84375436f75
Zihad Rizky Edwin Fikri <zihadrizkyef@gmail.com>
2018-03-05 14:53:52 +0700

    I don't know what update did i just do :D

--- app/src/main/java/zihadrizkyef/com/edittextformoney/MainActivity.kt
+++ app/src/main/java/zihadrizkyef/com/edittextformoney/MainActivity.kt
@@ -2,9 +2,6 @@ package zihadrizkyef.com.edittextformoney
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.text.Editable
-import android.text.TextWatcher
-import android.util.Log
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -13,71 +10,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        etMoney.setText("Rp ")
-        etMoney.addTextChangedListener(object: TextWatcher {
-            /**
-             * Text to show before money number
-             */
-            val moneyPrefix = "Rp "
-
-
-            //DONT EDIT FIELD BELOW
-            var prefString = ""
-            var prefCursorPos = 0
-            val startEditablePos = moneyPrefix.length
-
-            override fun afterTextChanged(s: Editable?) {}
-            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
-                prefString = s.toString()
-                prefCursorPos = etMoney.selectionStart
-            }
-            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-                s?.let {
-                    etMoney.removeTextChangedListener(this)
-
-                    var cursorPos = etMoney.selectionStart
-
-                    //prevent user to write or delete in the $moneyPrefix text
-                    if (prefCursorPos < startEditablePos || (cursorPos < prefCursorPos && cursorPos < startEditablePos)) {
-                        etMoney.setText(prefString)
-                        etMoney.setSelection(prefCursorPos)
-                    } else {
-                        //delete money prefix
-                        var filteredText = it.substring(moneyPrefix.length)
-                        cursorPos = Math.max(0, cursorPos - moneyPrefix.length)
-
-                        //delete all dot
-                        for (i in cursorPos - 1 downTo 0) {
-                            if (filteredText[i] == '.') {
-                                cursorPos -= 1
-                            }
-                        }
-                        filteredText = filteredText.replace(".", "")
-
-                        //add new dot
-                        val dotBuilder = StringBuilder(filteredText)
-                        if (filteredText.length > 3) {
-                            for (i in filteredText.length - 3 downTo 1 step 3) {
-                                if (i < cursorPos) {
-                                    cursorPos += 1
-                                }
-                                dotBuilder.insert(i, ".")
-                            }
-                        }
-                        filteredText = dotBuilder.toString()
-
-                        //add money prefix
-                        filteredText = moneyPrefix + filteredText
-                        cursorPos += moneyPrefix.length
-
-                        Log.i("AOEU", "length ${filteredText.length} pos $cursorPos")
-                        etMoney.setText(filteredText)
-                        etMoney.setSelection(cursorPos)
-                    }
-
-                    etMoney.addTextChangedListener(this)
-                }
-            }
-        })
+        etMoney.addTextChangedListener(TWMoney(etMoney, "Rp ", true))
     }
 }
---
+++ app/src/main/java/zihadrizkyef/com/edittextformoney/TWMoney.kt
@@ -0,0 +1,91 @@
+package zihadrizkyef.com.edittextformoney
+
+import android.annotation.SuppressLint
+import android.text.Editable
+import android.text.TextWatcher
+import android.widget.EditText
+
+/**
+ * بِسْمِ اللهِ الرَّحْمٰنِ الرَّحِيْمِ
+ * Created by zihadrizkyef on 05/03/18.
+ */
+
+/**
+ * TextWatcher for EditText which is containing money formatted text
+ *
+ * @param editText : The edit text which is applying this text watcher
+ * @param moneyPrefix : Text to show before money text. Might be "$ ", "Rp ", etc.
+ * @param showHint : Wheter to show hint or not. If not then moneyPrefix will be shown
+ */
+class TWMoney(val editText: EditText, var moneyPrefix: String, var showHint: Boolean) : TextWatcher {
+    private var prefString = ""
+    private var prefCursorPos = 0
+    private val startEditablePos = moneyPrefix.length
+
+    init {
+        if (editText.text.isEmpty()) {
+            if (!showHint) {
+                editText.setText(moneyPrefix)
+            }
+        }
+    }
+
+    override fun afterTextChanged(s: Editable?) {}
+    override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+        prefString = s.toString()
+        prefCursorPos = editText.selectionStart
+    }
+
+    @SuppressLint("SetTextI18n")
+    override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+        s?.let {
+            editText.removeTextChangedListener(this)
+
+            var cursorPos = editText.selectionStart
+
+            if (prefString.isEmpty() && showHint) {
+                editText.setText(moneyPrefix + it.toString())
+                editText.setSelection(editText.text.length)
+            } else if (it.toString() == moneyPrefix && showHint) {
+                editText.setText("")
+            } else if (prefCursorPos < startEditablePos || (cursorPos < prefCursorPos && cursorPos < startEditablePos)) {
+                //prevent user to write or delete in the $moneyPrefix text
+                editText.setText(prefString + "0")
+                editText.setSelection(prefCursorPos)
+            } else {
+                //delete money prefix
+                var filteredText = it.substring(moneyPrefix.length)
+                cursorPos = Math.max(0, cursorPos - moneyPrefix.length)
+
+                //delete all dot
+                for (i in cursorPos - 1 downTo 0) {
+                    if (filteredText[i] == '.') {
+                        cursorPos -= 1
+                    }
+                }
+                filteredText = filteredText.replace(".", "")
+
+                //add new dot
+                val dotBuilder = StringBuilder(filteredText)
+                if (filteredText.length > 3) {
+                    for (i in filteredText.length - 3 downTo 1 step 3) {
+                        if (i < cursorPos) {
+                            cursorPos += 1
+                        }
+                        dotBuilder.insert(i, ".")
+                    }
+                }
+                filteredText = dotBuilder.toString()
+
+                //add money prefix
+                filteredText = moneyPrefix + filteredText
+                cursorPos += moneyPrefix.length
+
+                editText.setText(filteredText)
+                editText.setSelection(cursorPos)
+            }
+
+            editText.addTextChangedListener(this)
+        }
+    }
+}
